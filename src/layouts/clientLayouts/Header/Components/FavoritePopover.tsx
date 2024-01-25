31395012f33de9eacb8a50520e77cc3ed1d15555
dattgt <trangiathanhdat02092002@gmail.com>
2024-01-25 08:19:50 +0700

    UI: Add NotificationPopover, MyLearningPopover, FavoritePopover

--- src/layouts/clientLayouts/Header/Components/FavoritePopover.tsx
+++ src/layouts/clientLayouts/Header/Components/FavoritePopover.tsx
@@ -1,5 +1,25 @@
-const FavoritePopover = () => {
-    return <div>FavoritePopover</div>;
-};
+import { List, Typography } from 'antd';
 
+const FavoritePopover = () => {
+  const notifications = [
+    { id: 1, content: 'Chưa có khóa học yêu thích nào' },
+  ];
+  return (
+    <div className='w-[300px]'>
+      <div className='p-[5px]'>
+        <Typography.Text className='text-xl font-bold'> Khóa học yêu thích </Typography.Text>
+      </div>
+      <div className='max-h-[300px] overflow-auto'>
+        <List
+          dataSource={notifications}
+          renderItem={(item) => (
+            <List.Item key={item.id}>
+              <Typography.Text>{item.content}</Typography.Text>
+            </List.Item>
+          )}
+        />
+      </div>
+    </div>
+  );
+};
 export default FavoritePopover;
