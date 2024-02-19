b80716457c830c6c042adf64c2633d6ebe2786ed
thanhlong109 <thinghiemminiworld@gmail.com>
2024-02-19 07:51:13 +0700

    fix warning

--- src/layouts/clientLayouts/Footer.tsx
+++ src/layouts/clientLayouts/Footer.tsx
@@ -1,47 +1,46 @@
-import React from "react";
-import FacebookIcon from "@mui/icons-material/Facebook";
-import InstagramIcon from "@mui/icons-material/Instagram";
-import TwitterIcon from "@mui/icons-material/Twitter";
-import YouTubeIcon from "@mui/icons-material/YouTube";
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import YouTubeIcon from '@mui/icons-material/YouTube';
 
 const Footer = () => {
-  return (
-    <div className="h-full py-10 bg-black mt-4">
-      <div className="flex justify-center">
-        <div className="space-y-6">
-          <div className="flex justify-start space-x-5 text-white">
-            <FacebookIcon fontSize="large" />
-            <InstagramIcon fontSize="large" />
-            <TwitterIcon fontSize="large" />
-            <YouTubeIcon fontSize="large" />
-          </div>
+    return (
+        <div className="mt-4 h-full bg-black py-10">
+            <div className="flex justify-center">
+                <div className="space-y-6">
+                    <div className="flex justify-start space-x-5 text-white">
+                        <FacebookIcon fontSize="large" />
+                        <InstagramIcon fontSize="large" />
+                        <TwitterIcon fontSize="large" />
+                        <YouTubeIcon fontSize="large" />
+                    </div>
 
-          <div className="grid grid-cols-2 gap-0 sm:grid-cols-4 sm:gap-4">
-            <div className="space-y-2 text-gray-600">
-              <p>Mô tả khóa học</p>
-              <p>Quan hệ với nhà đầu tư</p>
-              <p>Thông báo pháp lí</p>
+                    <div className="grid grid-cols-2 gap-0 sm:grid-cols-4 sm:gap-4">
+                        <div className="space-y-2 text-gray-600">
+                            <p>Mô tả khóa học</p>
+                            <p>Quan hệ với nhà đầu tư</p>
+                            <p>Thông báo pháp lí</p>
+                        </div>
+                        <div className="space-y-2 text-gray-600">
+                            <p>Trung tâm trợ giúp</p>
+                            <p>Việc làm</p>
+                            <p>Tùy chọn cookie</p>
+                        </div>
+                        <div className="space-y-2 text-gray-600">
+                            <p>Thẻ quà tặng</p>
+                            <p>Điều khoản sử dụng</p>
+                            <p>Thông tin doanh nghiệp</p>
+                        </div>
+                        <div className="space-y-2 text-gray-600">
+                            <p>Trung tâm đa phương tiện</p>
+                            <p>Quyền riêng tư</p>
+                            <p>Liên hệ với chúng tôi</p>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className="space-y-2 text-gray-600">
-              <p>Trung tâm trợ giúp</p>
-              <p>Việc làm</p>
-              <p>Tùy chọn cookie</p>
-            </div>
-            <div className="space-y-2 text-gray-600">
-              <p>Thẻ quà tặng</p>
-              <p>Điều khoản sử dụng</p>
-              <p>Thông tin doanh nghiệp</p>
-            </div>
-            <div className="space-y-2 text-gray-600">
-              <p>Trung tâm đa phương tiện</p>
-              <p>Quyền riêng tư</p>
-              <p>Liên hệ với chúng tôi</p>
-            </div>
-          </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Footer;
