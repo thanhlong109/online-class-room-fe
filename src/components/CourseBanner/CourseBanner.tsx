d10cf691b4bca7f14db8616dfea9be9b0d587c2d
anlehoang6789 <anlehoang6789@gmail.com>
2024-03-16 23:06:52 +0700

    feat: call api rating course for Course Card and Course Banner

--- src/components/CourseBanner/CourseBanner.tsx
+++ src/components/CourseBanner/CourseBanner.tsx
@@ -3,6 +3,9 @@ import StarIcon from '@mui/icons-material/Star';
 import { formatNumberWithCommas } from '../../utils/NumberFormater';
 import { Course } from '../../types/Course.type';
 import { Skeleton } from 'antd';
+import { useEffect, useState } from 'react';
+import { useGetRatingCourseQuery } from '../../services/ratingCourse.services';
+import { useCountStudentPerCourseQuery } from '../../services/course.services';
 
 const StyledRating = styled(Rating)({
     '& .MuiRating-icon': {
@@ -22,9 +25,39 @@ interface Props {
 }
 
 const CourseBanner = ({ isLoading, course }: Props) => {
-    const starRating = 3.8;
-    const totalRating = formatNumberWithCommas(9999);
-    const totalStudentJoin = formatNumberWithCommas(999999);
+    // const starRating = 3.8;
+    // const totalRating = formatNumberWithCommas(9999);
+    // const totalStudentJoin = formatNumberWithCommas(999999);
+    const [ratingData, setRatingData] = useState<{ averageRating: number; ratingCount: number }>({
+        averageRating: 0,
+        ratingCount: 0,
+    });
+    const { data: rating, isSuccess } = useGetRatingCourseQuery(course?.courseId || 0);
+    useEffect(() => {
+        if (isSuccess && rating) {
+            setRatingData({
+                averageRating: rating.averageRating,
+                ratingCount: rating.ratingCount,
+            });
+        }
+    }, [isSuccess, rating]);
+
+    const [totalStudents, setTotalStudents] = useState<number>(0);
+    const { data: courseData, isError } = useCountStudentPerCourseQuery();
+    useEffect(() => {
+        if (courseData && courseData.length > 0) {
+            // Lấy totalStudents từ dữ liệu câu truy vấn
+            const courseWithMatchingId = courseData.find(
+                (item) => item.courseId === course.courseId,
+            );
+            setTotalStudents(courseWithMatchingId?.totalStudents || 0);
+        }
+    }, [courseData]);
+
+    if (isError) return <div>Error fetching data</div>;
+
+    if (!courseData || courseData.length === 0) return <div>No data available</div>;
+
     return (
         <>
             {isLoading && <Skeleton active />}
@@ -34,17 +67,22 @@ const CourseBanner = ({ isLoading, course }: Props) => {
                         <h1 className="text-3xl font-bold ">{course.title}</h1>
                         <h2 className="mt-2 text-lg"></h2>
                         <div className="mt-2 flex h-8 items-center gap-2 text-[#f69c08]">
-                            <span className="text-sm">{starRating}</span>
+                            <span className="text-sm">{ratingData.averageRating}</span>
                             <StyledRating
                                 name="half-rating-read"
-                                defaultValue={starRating}
+                                value={ratingData.averageRating}
+                                defaultValue={0}
                                 precision={0.1}
                                 emptyIcon={<StarIcon fontSize="inherit" />}
                                 size="small"
                                 readOnly
                             />
-                            <span className="text-sm text-white">({totalRating} đánh giá)</span>
-                            <span className="text-sm font-medium">{totalStudentJoin} học sinh</span>
+                            <span className="text-sm text-white">
+                                ({ratingData.ratingCount} đánh giá)
+                            </span>
+                            <span className="text-sm font-medium">
+                                {formatNumberWithCommas(totalStudents)} học sinh
+                            </span>
                         </div>
                     </div>
                 </div>
